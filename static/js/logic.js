a954520b2d02586e28081f94369f6665ce300137
xlivix <oliviabryanttt@gmail.com>
2023-06-09 13:28:17 -0400

    olivia update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Get all filter links
-    const filterLinks = document.querySelectorAll('.panel a');
+    const filterLinks = document.querySelectorAll('.filters a');
     // Add click event listener to each filter link
     filterLinks.forEach((link) => {
         link.addEventListener('click', (event) => {
@@ -14,13 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Populate the table with the new data
                 data.forEach((player) => {
                     const row = document.createElement('tr');
-                    row.innerHTML = `
-                        <td>${player.long_name}</td>
-                        <td>${player.age}</td>
-                        <td>${player.overall}</td>
-                        <td>${player.club}</td>
-                        <td>${player.nationality}</td>
-                    `;
+                    row.innerHTML = `<td>${player.long_name}</td><td>${player.age}</td><td>${player.overall}</td><td>${player.club}</td><td>${player.nationality}</td>`;
                     tableBody.appendChild(row);
                 });
             }).catch((error) => {
