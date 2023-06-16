313b980d89dd4f88f66ef1f88dba95f60358644f
xlivix <oliviabryanttt@gmail.com>
2023-06-16 09:57:30 -0400

    update

--- static/js/map.js
+++ static/js/map.js
@@ -1,7 +1,7 @@
 function displayMap() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map', { attributionControl: false }).setView([51.505, -0.09], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         maxZoom: 18,
     }).addTo(map);
 }
